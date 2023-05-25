9bf10053d295fa8555389a376106152f4dab3e9c
TheBigBurd <55106453+thebigburd@users.noreply.github.com>
2023-05-25 15:11:31 +0100

    Initial Commit. Packages and Dependencies. Test and Implementation of a generic card class.

---
+++ src/card/CardImpl.ts
@@ -0,0 +1,35 @@
+import { Card } from './Card';
+
+export class CardImpl implements Card {
+    
+    rank: string;
+    suit: string;
+    
+    constructor(rank: string, suit: string) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+     get cardRank() : string {
+        return this.rank;
+    }
+
+    set cardRank(rank: string) {
+        this.rank = rank;
+        
+    }
+
+    get cardSuit() : string {
+        return this.suit;
+    }
+
+    set cardSuit(suit: string){
+        this.suit = suit;
+    }
+
+
+    getCardName() : string {
+        return `${this.rank} of ${this.suit}`;
+    }
+    
+}
---
+++ test/card/Card.test.ts
@@ -0,0 +1,57 @@
+import { expect } from "chai";
+import { CardImpl } from '../../src/card/CardImpl';
+
+type CardConstructor = new (rank: string, suit: string) => CardImpl;
+
+export function testCard(Card: CardConstructor) {
+    describe("General Card Tests", () => {
+        it("Should be instantiated with a rank and a suit", () => {
+            // Call Constructor to instantiate Card
+            const card = new Card("Ace", "Spades");
+
+            // Assert
+            expect(card.rank).to.equal("Ace");
+
+            expect(card.suit).to.equal("Spades");
+
+            expect(card.getCardName()).to.equal("Ace of Spades");
+
+        });
+
+        it("Should change its name when Rank is modified.", () => {
+            // Call Constructor to instantiate Card
+            const card = new Card("Ace", "Spades");
+            expect(card.getCardName()).to.equal("Ace of Spades");
+
+            // Act
+            card.rank = "King";
+
+            // Assert
+            expect(card.rank).to.equal("King");
+
+            expect(card.getCardName()).to.equal("King of Spades");
+
+        });
+
+        it("Should change its name when Suit is modified", () => {
+            // Call Constructor to instantiate Card
+            let card = new Card("Ace", "Spades");
+            expect(card.getCardName()).to.equal("Ace of Spades");
+
+            // Act
+            card.suit = "Hearts";
+
+            // Assert
+            expect(card.suit).to.equal("Hearts");
+
+            expect(card.getCardName()).to.equal("Ace of Hearts");
+        });
+    })
+}
+
+
+// Create an instance of CardImpl constructor function
+const cardImplConstructor = CardImpl as CardConstructor;
+
+// Call the testCard function with the CardImpl constructor
+testCard(cardImplConstructor);
