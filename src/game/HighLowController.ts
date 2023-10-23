a08ca9060b7cf705c1d3bb2d3fbf0c0b05c31426
TheBigBurd <55106453+thebigburd@users.noreply.github.com>
2023-10-23 16:13:24 +0100

    Implemented High or Low Controller class

---
+++ src/game/HighLowController.ts
@@ -0,0 +1,172 @@
+import { CardImpl } from "../card/CardImpl";
+import { DeckImpl } from "../deck/DeckImpl";
+import { GameController } from "./GameController";
+
+const GAMESTATE = {
+	PLAYER: "PLAYER",
+	CORRECT: "CORRECT",
+	RESULT: "RESULT",
+} as const;
+
+const WINTYPE = {
+	WIN: "WIN",
+	SURRENDER: "SURRENDER",
+	LOSE: "LOSE",
+} as const;
+
+type GameState = typeof GAMESTATE[keyof typeof GAMESTATE]
+type WinType = typeof WINTYPE[keyof typeof WINTYPE]
+
+export class HighLowController implements GameController {
+	maxplayers: number;
+	gameState: GameState;
+	winType: WinType;
+	private playingDeck: DeckImpl;
+	private playerCard: CardImpl;
+	private nextCard: CardImpl;
+	private streak: number;
+	private multiplier: number;
+
+	constructor(deckCount: number, multiplier) {
+		this.maxplayers = 1;
+		this.streak = 0;
+		this.multiplier = multiplier;
+
+		this.playingDeck = new DeckImpl([], 0);
+		for (let i = 0; i < deckCount; i++) {
+			this.playingDeck.addCards();
+		}
+
+	}
+
+	startGame() {
+		this.playingDeck.shuffle();
+
+		this.playerCard = this.playingDeck.drawCard();
+		this.nextCard = this.playingDeck.drawCard();
+
+		this.gameState = GAMESTATE.PLAYER;
+	}
+
+	playerAct(higher: boolean) {
+		if (this.gameState !== GAMESTATE.PLAYER) {
+			throw new Error("It's not the player's turn.");
+		}
+
+		const cardValue = this.getCardValue(this.playerCard);
+		const nextCardValue = this.getCardValue(this.nextCard);
+
+		// If next card is higher than player's card
+		if (cardValue < nextCardValue && higher) {
+			this.streak += 1;
+			this.gameState = GAMESTATE.CORRECT;
+		}
+		// If next card is lower than player's card
+		else if (cardValue > nextCardValue && !higher) {
+			this.streak += 1;
+			this.gameState = GAMESTATE.CORRECT;
+		}
+		else {
+			this.gameState = GAMESTATE.RESULT;
+			this.winType = WINTYPE.LOSE;
+		}
+	}
+
+	continue() {
+		if (this.gameState !== GAMESTATE.CORRECT) {
+			throw new Error("It is not time to decide. This option should not be available.");
+		}
+
+		// Resets and shuffles the deck after 10 Wins to prevent Card Counting.
+		if (this.streak % 10 === 0) {
+			this.playingDeck.clearDeck();
+			this.playingDeck.addCards();
+			this.playingDeck.shuffle;
+		}
+
+		this.playerCard = this.nextCard;
+		this.nextCard = this.playingDeck.drawCard();
+
+		this.gameState = GAMESTATE.PLAYER;
+	}
+
+	surrender() {
+		if (this.gameState === GAMESTATE.CORRECT) {
+			this.winType = WINTYPE.WIN;
+			this.gameState = GAMESTATE.RESULT;
+		}
+		else if (this.gameState === GAMESTATE.PLAYER) {
+			this.winType = WINTYPE.SURRENDER;
+			this.gameState = GAMESTATE.RESULT;
+		}
+		else {
+			throw new Error("This option shold not be available. It is not time to decide.");
+		}
+	}
+
+	getResult() {
+		if (this.winType === WINTYPE.WIN) {
+			return ("You leave taking the whole pot!");
+		}
+
+		if (this.winType === WINTYPE.SURRENDER) {
+			return ("You surrendered, forfeiting half the pot!");
+		}
+
+		return ("Incorrect. You have lost!");
+	}
+
+	getPlayerCard(): CardImpl {
+		return this.playerCard;
+	}
+
+	getNextCard(): CardImpl {
+		return this.nextCard;
+	}
+
+	getStreak(): number {
+		return this.streak;
+	}
+
+	setStreak(num: number) {
+		this.streak = num;
+	}
+
+	getPlayingDeck(): DeckImpl {
+		return this.playingDeck;
+	}
+
+	getCardValue(card: CardImpl): number {
+		switch (card.cardRank) {
+		case "Ace":
+			return 1;
+		case "Two":
+			return 2;
+		case "Three":
+			return 3;
+		case "Four":
+			return 4;
+		case "Five":
+			return 5;
+		case "Six":
+			return 6;
+		case "Seven":
+			return 7;
+		case "Eight":
+			return 8;
+		case "Nine":
+			return 9;
+		case "Ten":
+			return 10;
+		case "Jack":
+			return 11;
+		case "Queen":
+			return 12;
+		case "King":
+			return 13;
+		default:
+			throw new Error(`Invalid card rank: ${card.rank}`);
+		}
+	}
+
+}
---
+++ test/game/HighLowController.test.ts
@@ -0,0 +1,181 @@
+import { expect } from "chai";
+import { HighLowController } from "../../src/game/HighLowController";
+import { DeckImpl } from "../../src/deck/DeckImpl";
+import { instance, mock, when } from "ts-mockito";
+import { CardImpl } from "../../src/card/CardImpl";
+
+
+describe("High Low Controller Test", () => {
+	it("Should change the Game State to CORRECT, and increment streak, if the player predicts correctly that the next card is higher.", () => {
+		// Setup
+		// Create mocks
+		const deckImplMock: DeckImpl = mock(DeckImpl);
+		const deckInstance = instance(deckImplMock);
+
+		// Stub the Draw Card method of mock.
+		when(deckImplMock.drawCard())
+			.thenReturn(new CardImpl("Five", "Hearts"))
+			.thenReturn(new CardImpl("Ten", "Clubs"));
+
+		// Create the game instance with the mocked deck
+		const game = new HighLowController(1, 0.3);
+		game["playingDeck"] = deckInstance;
+
+		game.startGame();
+
+		// Act
+		game.playerAct(true);
+
+		// Assert
+		expect(game.gameState).to.equal("CORRECT");
+		expect(game.getStreak()).to.be.equal(1);
+
+	});
+
+	it("Should change the Game State to CORRECT, and increment streak, if the player predicts correctly that the next card is lower", () => {
+		// Setup
+		// Create mocks
+		const deckImplMock: DeckImpl = mock(DeckImpl);
+		const deckInstance = instance(deckImplMock);
+
+		// Stub the Draw Card method of mock.
+		when(deckImplMock.drawCard())
+			.thenReturn(new CardImpl("Ten", "Clubs"))
+			.thenReturn(new CardImpl("Five", "Hearts"));
+
+		// Create the game instance with the mocked deck
+		const game = new HighLowController(1, 0.3);
+		game["playingDeck"] = deckInstance;
+
+		game.startGame();
+
+		// Act
+		game.playerAct(false);
+
+		// Assert
+		expect(game.gameState).to.equal("CORRECT");
+		expect(game.getStreak()).to.be.equal(1);
+	});
+
+	it("Should change the Game State to RESULT, and Win Type to LOSE, if the player predicts incorrectly that the next card is higher.", () => {
+		// Setup
+		// Create mocks
+		const deckImplMock: DeckImpl = mock(DeckImpl);
+		const deckInstance = instance(deckImplMock);
+
+		// Stub the Draw Card method of mock.
+		when(deckImplMock.drawCard())
+			.thenReturn(new CardImpl("Ten", "Clubs"))
+			.thenReturn(new CardImpl("Five", "Hearts"));
+
+		// Create the game instance with the mocked deck
+		const game = new HighLowController(1, 0.3);
+		game["playingDeck"] = deckInstance;
+
+		game.startGame();
+
+		// Act
+		game.playerAct(true);
+
+		// Assert
+		expect(game.gameState).to.equal("RESULT");
+		expect(game.winType).to.equal("LOSE");
+		expect(game.getStreak()).to.be.equal(0);
+	});
+
+	it("Should change the Game State to RESULT, and Win Type to LOSE, if the player predicts incorrectly that the next card is lower.", () => {
+		// Setup
+		// Create mocks
+		const deckImplMock: DeckImpl = mock(DeckImpl);
+		const deckInstance = instance(deckImplMock);
+
+		// Stub the Draw Card method of mock.
+		when(deckImplMock.drawCard())
+			.thenReturn(new CardImpl("Ace", "Hearts"))
+			.thenReturn(new CardImpl("King", "Clubs"));
+
+		// Create the game instance with the mocked deck
+		const game = new HighLowController(1, 0.3);
+		game["playingDeck"] = deckInstance;
+
+		game.startGame();
+
+		// Act
+		game.playerAct(false);
+
+		// Assert
+		expect(game.gameState).to.equal("RESULT");
+		expect(game.winType).to.equal("LOSE");
+		expect(game.getStreak()).to.be.equal(0);
+	});
+
+	it("Should change the Game State to RESULT, and Win Type to WIN, if they choose to quit between rounds.", () => {
+		// Setup
+		// Create mocks
+		const deckImplMock: DeckImpl = mock(DeckImpl);
+		const deckInstance = instance(deckImplMock);
+
+		// Stub the Draw Card method of mock.
+		when(deckImplMock.drawCard())
+			.thenReturn(new CardImpl("Ace", "Hearts"))
+			.thenReturn(new CardImpl("King", "Clubs"));
+
+		// Create the game instance with the mocked deck
+		const game = new HighLowController(1, 0.3);
+		game["playingDeck"] = deckInstance;
+
+		game.startGame();
+		game.playerAct(true);
+
+		// Act
+		game.surrender();
+
+		// Assert
+		expect(game.gameState).to.equal("RESULT");
+		expect(game.winType).to.equal("WIN");
+		expect(game.getStreak()).to.be.equal(1);
+	});
+
+	it("Should change the Game State to RESULT, and Win Type to SURRENDER, if they choose to quit during a round.", () => {
+		// Setup
+		// Create mocks
+		const deckImplMock: DeckImpl = mock(DeckImpl);
+		const deckInstance = instance(deckImplMock);
+
+		// Stub the Draw Card method of mock.
+		when(deckImplMock.drawCard())
+			.thenReturn(new CardImpl("Ace", "Hearts"))
+			.thenReturn(new CardImpl("King", "Clubs"));
+
+		// Create the game instance with the mocked deck
+		const game = new HighLowController(1, 0.3);
+		game["playingDeck"] = deckInstance;
+
+		game.startGame();
+
+		// Act
+		game.surrender();
+
+		// Assert
+		expect(game.gameState).to.equal("RESULT");
+		expect(game.winType).to.equal("SURRENDER");
+	});
+
+	it("Should reset and shuffle the deck when the player's win streak reaches a multiple of 10.", () => {
+		// Setup
+		const game = new HighLowController(1, 0.3);
+		game.setStreak(10);
+		game.startGame();
+		game.gameState = "CORRECT";
+
+		// Assert Before
+		expect(game.getPlayingDeck().currentIndex).to.equal(2);
+
+		// Act
+		game.continue();
+
+		// Assert
+		expect(game.getPlayingDeck().currentIndex).to.equal(1);
+	});
+
+});
